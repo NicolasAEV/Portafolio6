122ce5c2c01914112ee9969b9e7bdf9f4b3be9b4
NicolasAEV <NicolasEscobar.v@hotmail.com>
2023-03-28 22:31:23 -0300

    env

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,18 @@
 import {Sequelize}  from "sequelize";
-export const sequelize  = new Sequelize ('name bd','usuario','password',{
-    host : '',
+
+    const DB_DATABASE = process.env.DB_DATABASE || 'portafolio';
+    const DB_USER = process.env.DB_USER || 'postgres';
+    const DB_HOST = process.env.DB_HOST || 'localhost';
+    const DB_PASSWORD = process.env.DB_PASSWORD || 'Jasonpia';
+
+
+
+
+
+export const sequelize  = new Sequelize (DB_DATABASE,DB_USER,DB_PASSWORD,{
+    host : DB_HOST,
     dialect:'postgres'
 })
 
 
+    
