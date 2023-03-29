2741b8856bad7f04ca55d397c3913cbeccd88357
NicolasAEV <NicolasEscobar.v@hotmail.com>
2023-03-28 23:26:20 -0300

    ya

--- src/db/db.js
+++ src/db/db.js
@@ -2,13 +2,13 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 import {Sequelize}  from "sequelize";
 
-    // const DB_DATABASE = process.env.DB_DATABASE;
-    // const DB_USER = process.env.DB_USER;
-    // const DB_HOST = process.env.DB_HOST;
-    // const DB_PASSWORD = process.env.DB_PASSWORD;
+    const DB_DATABASE = process.env.DB_DATABASE;
+    const DB_USER = process.env.DB_USER;
+    const DB_HOST = process.env.DB_HOST;
+    const DB_PASSWORD = process.env.DB_PASSWORD;
 
-export const sequelize  = new Sequelize ('portafolio','postgres','Jasonpia0',{
-    host :'localhost',
+export const sequelize  = new Sequelize (DB_DATABASE ,DB_USER,DB_PASSWORD,{
+    host :DB_HOST,
     dialect:'postgres'
 })
 
